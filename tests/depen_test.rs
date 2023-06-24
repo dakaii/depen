a3bb7d5cc10b6ad8bbdcdea67feb264b01ab7a06
Daiki Nakashita <daiki815@gmail.com>
2023-06-24 10:49:14 +0900

    git a new test it_resolves_dependency_order

--- tests/depen_test.rs
+++ tests/depen_test.rs
@@ -1,7 +1,29 @@
 use depen::{resolve_dependency_order, Dep, DepName};
 
 #[test]
-fn it_resolves_dependency_order() {
+fn it_resolves_dependency_order_without_transitive_deps() {
+    let example_dep = Dep {
+        name: DepName::new("example_dep".to_owned()),
+        transitive_deps: Vec::new(),
+    };
+    let another_dep = Dep {
+        name: DepName::new("another_dep".to_owned()),
+        transitive_deps: Vec::new(),
+    };
+
+    let mut vec = Vec::new();
+    vec.push(example_dep.to_owned());
+    vec.push(another_dep.to_owned());
+    let deps = resolve_dependency_order(vec);
+
+    let mut correct_answer = Vec::new();
+    correct_answer.push(example_dep.name);
+    correct_answer.push(another_dep.name);
+    assert_eq!(correct_answer, deps);
+}
+
+#[test]
+fn it_resolves_complex_dependency_order() {
     let super_super_deep = Dep {
         name: DepName::new("super_super_deep".to_owned()),
         transitive_deps: Vec::new(),
@@ -22,15 +44,15 @@ fn it_resolves_dependency_order() {
         name: DepName::new("one_more_dep".to_owned()),
         transitive_deps: Vec::from([another_dep.to_owned(), super_deep.to_owned()]),
     };
-    let good_dep = Dep {
-        name: DepName::new("good_dep".to_owned()),
+    let amazing_dep = Dep {
+        name: DepName::new("amazing_dep".to_owned()),
         transitive_deps: Vec::from([another_dep.to_owned(), one_more_dep.to_owned()]),
     };
 
     let mut vec = Vec::new();
-    vec.push(example_dep.to_owned());
-    vec.push(good_dep.to_owned());
     vec.push(another_dep.to_owned());
+    vec.push(amazing_dep.to_owned());
+    vec.push(example_dep.to_owned());
     let deps = resolve_dependency_order(vec);
 
     let mut correct_answer = Vec::new();
@@ -39,7 +61,7 @@ fn it_resolves_dependency_order() {
     correct_answer.push(example_dep.name);
     correct_answer.push(another_dep.name);
     correct_answer.push(one_more_dep.name);
-    correct_answer.push(good_dep.name);
+    correct_answer.push(amazing_dep.name);
     assert_eq!(correct_answer, deps);
 }
 
